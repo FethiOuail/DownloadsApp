572a61d1442d00f4c62f281f497f22a24cfc17f1
FethiOuail <anwarinfo26@gmail.com>
2020-08-20 00:33:37 +0100

    first commits

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import sys
+from typing import re
 
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.uic import loadUiType
 
+import urllib.request
+
 import os
 from os import path
 
 # FORM_CLASS = loadUiType(path.join(path.dirname(__file__), "main.ui"))
+from setuptools._vendor.six import print_
+
 ui, _ = loadUiType('main.ui')
 
 
@@ -18,21 +23,43 @@ class MainApp(QMainWindow, ui):
 		QMainWindow.__init__(self)
 		self.setupUi(self)
 		self.Hundel_Ui()
+		self.Hundel_Buttons()
 
 	def Hundel_Buttons(self):
-		pass
+		self.btnDownload.clicked.connect(self.Download)
 
 	def hundelBrowse(self):
 		pass
 
-	def Hundle_Progress(self):
-		pass
+	def Hundle_Progress(self, blocknum, blocksize, totalesize):
+
+		read = blocknum * blocksize
+
+		if totalesize > 0:
+			percent = read * 100 / totalesize
+			self.progressBar.setValue(int(percent))
+			QApplication.processEvents()  # Fix Lag
 
 	def Download(self):
-		pass
+		url_d = self.urlEdit.text()
+		save_location = self.saveLocation.text()
+		print(url_d)
+		# F:/PycharmProjects/DownloadsApp/img3.jpg
+
+		try:
+			urllib.request.urlretrieve(url_d, save_location, self.Hundle_Progress)
+		except Exception :
+			QMessageBox.warning(self, "Downloads Error", " Error")
+			return
+
+		QMessageBox.information(self, "Download Completed", "The Download Finished")
+		self.progressBar.setValue(0)
+		self.urlEdit.setText('')
+		self.saveLocation.setText('')
 
 	def Hundel_Ui(self):
 		self.setWindowTitle("Downloader")
+		self.setFixedSize(600, 260)
 
 
 def main():
